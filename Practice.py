1251de04b3d845e748353f1ea4f6c3aea87bd90d
Esteban-gu <esteban2007alvarez@gmail.com>
2024-10-30 12:12:11 -0500

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,52 @@
+
+# # First Excercise
+
+# First_Answer = input("What is your favorite programming language? : ")
+
+# Answer = "Python" or "python"
+
+# if Answer == First_Answer:
+#     print('You love Python!')
+# else:
+#     print('Different Choice')
+
+# Second Excercise
+    
+# grade = int(input("What is your grade?: "))
+
+# # grade_scale= [0,100]
+
+# if grade >= 90:
+#     print('You have an A')
+# elif grade >= 80 and grade <= 89:
+#     print('You have a B')
+# elif grade >= 70 and grade <= 79:
+#     print('You have a C')
+# elif grade >= 60 and grade <= 69:
+#     print('You have a D')
+# elif grade > 60:
+#     print('You have an F')
+
+# # Third Excercise
+# user = input("What is your user?")
+
+# user == 'Admin'
+# logged_in = True
+# if user == 'Admin' and logged_in:
+#  print("Welcome admin!")
+# else:
+#  print("Bad Creds")
+
+# Fourth Excercise
+
+# a = [ 1, 2, 3, 4, 5 ]
+# b = [ 1, 2, 3, 4, 5 ]
+
+# print(id(a))
+
+# print(id(b))
+
+# if id(a) == id(b):
+#     print(True)
+# else:
+#     print(False)
